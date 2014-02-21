663f5868a6e036026f72702a9786ec4fb950aa61
Eric Saxby & James Hart <pair+sax+james@wanelo.com>
2014-02-21 15:53:48 -0800

    Add test cookbook through Berkshelf

---
+++ Berksfile
@@ -0,0 +1,5 @@
+site :opscode
+
+metadata
+
+cookbook 'test_nginx_site', path: 'test/integration/cookbooks/test_nginx_site'
---
+++ test/integration/cookbooks/test_nginx_site/metadata.rb
@@ -0,0 +1,9 @@
+name             'test_nginx_site'
+maintainer       'test'
+maintainer_email 'ops@wanelo.com'
+license          'MIT'
+description      'test cookbook for nginx site'
+long_description 'test cookbook for nginx site'
+version          '0.1.0'
+
+depends 'nginx'
