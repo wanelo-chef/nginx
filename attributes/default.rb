b40bad693f6b3406defcef3867d210c1b96ac11d
Eric Saxby <sax@livinginthepast.org>
2014-02-25 16:55:04 -0800

    formatting changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['nginx']['dir'] = "#{node['paths']['etc_dir']}/nginx"
-default['nginx']['log_dir'] = "/var/log/nginx"
+default['nginx']['log_dir'] = '/var/log/nginx'
 
 default['nginx']['user'] = "www"
 default['nginx']['group'] = node['nginx']['user']
@@ -8,27 +8,27 @@ default['nginx']['worker_processes'] = node['cpu'] && node['cpu']['total'] ? nod
 default['nginx']['binary'] = "#{node['paths']['sbin_dir']}/nginx"
 default['nginx']['sbin'] = node['paths']['sbin_dir']
 
-default['nginx']['pid'] = "/var/run/nginx.pid"
+default['nginx']['pid'] = '/var/run/nginx.pid'
 
-default['nginx']['gzip']              = "on"
-default['nginx']['gzip_http_version'] = "1.0"
-default['nginx']['gzip_comp_level']   = "2"
-default['nginx']['gzip_proxied']      = "any"
-default['nginx']['gzip_vary']         = "off"
-default['nginx']['gzip_buffers']      = "32 4k"
-default['nginx']['gzip_types']        = [
-  "text/plain",
-  "text/css",
-  "application/x-javascript",
-  "text/xml",
-  "application/xml",
-  "application/xml+rss",
-  "text/javascript",
-  "application/javascript",
-  "application/json"
+default['nginx']['gzip']              = 'on'
+default['nginx']['gzip_http_version'] = '1.0'
+default['nginx']['gzip_comp_level']   = '2'
+default['nginx']['gzip_proxied']      = 'any'
+default['nginx']['gzip_vary']         = 'off'
+default['nginx']['gzip_buffers']      = '32 4k'
+default['nginx']['gzip_types']        = %w[
+  text/plain
+  text/css
+  application/x-javascript
+  text/xml
+  application/xml
+  application/xml+rss
+  text/javascript
+  application/javascript
+  application/json
 ]
 
-default['nginx']['keepalive']          = "on"
+default['nginx']['keepalive']          = 'on'
 default['nginx']['keepalive_timeout']  = 65
 default['nginx']['worker_connections'] = 1024
 default['nginx']['worker_rlimit_nofile'] = nil
