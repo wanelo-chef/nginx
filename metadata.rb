b74e030f12ac60b2586f40b4c2cc2b21e2fe6a06
Eric Saxby <sax@livinginthepast.org>
2014-08-20 16:53:52 -0700

    Loosen smf cookbook requirements

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'ops@wanelo.com'
 license          'MIT'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '0.4.2'
 
 depends 'build-essential'
 depends 'paths', '~> 0.3'
 depends 'resource-control', '~> 0.1'
-depends 'smf', '~> 2.0.3'
+depends 'smf', '~> 2.1'
