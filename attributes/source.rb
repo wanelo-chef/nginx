0544b3b87b9dbb6c7937317916b21f9661978a4b
Eric Saxby <eric@wanelo.com>
2015-07-13 11:56:03 -0700

    Install nginx 1.8.0

--- attributes/source.rb
+++ attributes/source.rb
@@ -1,4 +1,4 @@
-default['nginx']['source']['version'] = '1.4.5'
+default['nginx']['source']['version'] = '1.8.0'
 default['nginx']['source']['mirror'] = 'http://nginx.org/download'
 default['nginx']['source']['url'] = "#{node['nginx']['source']['mirror']}/nginx-#{node['nginx']['source']['version']}.tar.gz"
 
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@wanelo.com'
 license          'MIT'
 description      'Installs/Configures nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.2'
+version          '0.4.3'
 
 depends 'build-essential'
 depends 'paths', '~> 0.3'
